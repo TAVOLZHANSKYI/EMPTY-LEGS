942621fc0de4ab80482e20a6e8dd9f6bb42c48ff
Nikoliy <39348231+TAVOLZHANSKYI@users.noreply.github.com>
2018-09-28 15:32:47 +0300

    Add files via upload

---
+++ рейсы/min.js
@@ -0,0 +1,61 @@
+
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+function myF() {
+    var popup = document.getElementById("myP");
+    popup.classList.toggle("show");
+}
+
+function col01() {
+    var popup = document.getElementById("col01");
+    popup.classList.toggle("show");
+}
+
+function col02() {
+    var popup = document.getElementById("col02");
+    popup.classList.toggle("show");
+}
+function col03() {
+    var popup = document.getElementById("col03");
+    popup.classList.toggle("show");
+}
+function col04() {
+    var popup = document.getElementById("col04");
+    popup.classList.toggle("show");
+}
+function col05() {
+    var popup = document.getElementById("col05");
+    popup.classList.toggle("show");
+}
+function col06() {
+    var popup = document.getElementById("col06");
+    popup.classList.toggle("show");
+}
+function col07() {
+    var popup = document.getElementById("col07");
+    popup.classList.toggle("show");
+}
+function col08() {
+    var popup = document.getElementById("col08");
+    popup.classList.toggle("show");
+}
+function col09() {
+    var popup = document.getElementById("col09");
+    popup.classList.toggle("show");
+}
+function col10() {
+    var popup = document.getElementById("col10");
+    popup.classList.toggle("show");
+}
+function col11() {
+    var popup = document.getElementById("col11");
+    popup.classList.toggle("show");
+}
+function col12() {
+    var popup = document.getElementById("col12");
+    popup.classList.toggle("show");
+}
